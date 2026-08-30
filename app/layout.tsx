01de2261454e763de3e3e29fa65c1f97874473ef
mrbodyfroy36-ui <mrbodyfroy36@gmail.com>
2026-08-30 10:37:10 -0700

    Create RootLayout component for app layout

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0, background:'#020617', color:'white'}}>{children}</body>
+    </html>
+  );
+}
